f74deb1419351c84f33f5c2fdcc2cdee6edd3b74
and <testermail35@gmail.com>
2019-12-06 11:13:25 +0300

    QA-1693

--- Config/Users.py
+++ Config/Users.py
@@ -181,6 +181,12 @@ class UserforDeleteOneMultisigAddress:
     email_password = "123QWEasdZXC"
     pincode = "7777"
 
+class UserforCheckAddWallets:
+    '''юзер для проверки наличия валют во вкладках addwallets'''
+    email = "testermail35+1693@gmail.com"
+    password = "12345678"
+    pincode = "7777"
+
 class CommonData:
     FWTContract = "0xf966bc1e1ab786830fd98a3f8f978940f8e5c13e"
     unsupportedSmartContract = "0x8b40761142b9aa6dc8964e61d0585995425c3d94"
